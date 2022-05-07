a081fe3874257b4b917208b5d30d9509cf57fec9
Charles Bayley <cbayley95@gmail.com>
2022-05-06 22:16:07 -0400

    Cleaned up stuff

--- euler/src/main/scala/Main.scala
+++ euler/src/main/scala/Main.scala
@@ -1,9 +1,8 @@
 import euler.Euler
 
 object Main extends App {
-  val showAll = true;
   Euler.problems()
-    .filter(p => showAll || p.solved)
+    .filter(p => p.solved)
     .map(p => p.toReportString())
     .map(r => "##############" + "\n" + r + "\n\n")
     .foreach(println)
